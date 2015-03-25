7cc69512964553ef3dbcc9684cf151189624ccd2
peace0phmind <peace0phmind@gmail.com>
2015-03-25 12:25:51 +0800

    add asset builtin

---
+++ modules/builtin/app/controllers/asset.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/tbud/bud/asset"
+	"github.com/tbud/tea/context"
+	"path/filepath"
+)
+
+type Assets struct {
+	*context.Context
+}
+
+func (a *Assets) At(path string, file string) {
+
+}
